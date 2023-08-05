55f097acbb5daf5004462f1c658a05c5f49e4b3f
Jeff Toppings <jeffrey.toppings@outlook.com>
2023-08-05 14:45:39 -0600

    feat: continue wiring up configureComponent

--- src/components/configure/configure.component.ts
+++ src/components/configure/configure.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-configure',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './configure.component.html',
-  styleUrls: ['./configure.component.scss']
+  styleUrls: ['./configure.component.scss'],
 })
 export class ConfigureComponent {
+  playerControls: FormControl[] = [new FormControl(), new FormControl()];
+  maxPlayers = 7;
+
+  handleAddNewPlayer(): void {
+    this.playerControls.push(new FormControl());
+  }
+
+  handleRemovePlayer(index: number): void {
+    this.playerControls.splice(index, 1);
+  }
+
+  handleStart(): void {
+    // TODO
+  }
 
 }
